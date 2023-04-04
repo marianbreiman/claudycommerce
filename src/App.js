5319c735623a1e6fc263063e791d32fbcd6ff2d4
“Marian <marianbreiman@gmail.com>
2023-04-04 19:08:45 -0300

    primer cambio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import { Login } from './Login';
+import "./App.scss";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Home } from "./components/Home/Home";
+import { Counter } from "./components/Counter/Counter";
+import { Trusted } from "./components/Trusted/Trusted";
+import { Footer } from "./components/Footer/Footer";
+import { ItemListContainer } from "./components/ItemList/ItemListContainer";
+
 function App() {
+  let greetings = "Hello, welcome again!";
+
   return (
     <div className="App">
-<Login />
-<h1>Esto es propio desde la app.js</h1>
+      <Navbar />
+      <Home />
+      <ItemListContainer greetings={greetings} />
+      <Trusted />
+      <Counter />
+      <Footer />
     </div>
   );
 }
